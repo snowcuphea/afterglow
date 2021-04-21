d57353734fe2789176eef5ec574698f07b983f01
halo2281@naver.com <halo2281@naver.com>
2021-04-21 17:37:00 +0900

    [EDIT] Added Entity Annotation
    유저 클래스에 Entity 어노테이션 추가

--- backend/src/main/java/ssafy/backend/afterglow/domain/User.java
+++ backend/src/main/java/ssafy/backend/afterglow/domain/User.java
@@ -4,10 +4,11 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
-// 유저
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,24 +17,16 @@ import javax.persistence.GenerationType;
 public class User {
 
     // 일련번호
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty("usr_id")
     private Integer usrId;
 
-    // 이메일
     @JsonProperty("usr_email")
     private String usrEmail;
-
-    // 비밀번호
     @JsonProperty("usr_pwd")
     private String usrPwd;
-
-    // 닉네임
     @JsonProperty("usr_nickname")
     private String usrNickname;
-
-    // 여행 중 여부
     @JsonProperty("usr_traveling_state")
     private Integer usrTravelingState;
 }
