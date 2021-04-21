cd11c75092612169d905987f1e64b162dbca6b83
halo2281@naver.com <halo2281@naver.com>
2021-04-21 14:10:49 +0900

    [ADD] Sample Controller 생성
    Reqeust 받을 예시 Controller 함수를 만듬

--- backend/src/main/java/ssafy/backend/afterglow/controller/RecordController.java
+++ backend/src/main/java/ssafy/backend/afterglow/controller/RecordController.java
@@ -1,4 +1,17 @@
 package ssafy.backend.afterglow.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ssafy.backend.afterglow.domain.Record;
+
+@RestController
+@RequestMapping("records")
 public class RecordController {
+    @GetMapping("/records")
+    public ResponseEntity<Record> sampleFunction(@PathVariable("recordId") Integer id){
+        return null;
+    }
 }
