f5e8ec8b67eb21fc1114fcda10590cdb501cdc4c
YeonggilGo <dudrlf1859@naver.com>
2021-04-21 11:03:31 +0900

    [ADD] exclude properties, add websecurity

---
+++ backend/src/main/java/ssafy/backend/afterglow/config/WebSecurityConfig.java
@@ -0,0 +1,7 @@
+package ssafy.backend.afterglow.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class WebSecurityConfig {
+}
