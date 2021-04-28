1c3c40159cf9fdf5935c99732a62c887ebe53e7f
halo2281@naver.com <halo2281@naver.com>
2021-04-28 13:38:58 +0900

    [ADD] Add Controller, Service

--- backend/src/main/java/ssafy/backend/afterglow/controller/RecordController.java
+++ backend/src/main/java/ssafy/backend/afterglow/controller/RecordController.java
@@ -1,22 +1,75 @@
 package ssafy.backend.afterglow.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ssafy.backend.afterglow.domain.Record;
+import org.springframework.web.bind.annotation.*;
+import ssafy.backend.afterglow.VO.*;
 import ssafy.backend.afterglow.service.RecordService;
 
 @RestController
-@RequestMapping("record")
+@RequestMapping("records")
 public class RecordController {
+    static final int SUCCESS = 1;
+    static final int FAIL = -1;
+
     @Autowired
     RecordService service;
 
-    @GetMapping("/records")
-    public ResponseEntity<Record> sampleFunction(@PathVariable("recordId") Integer id){
-        return null;
+    @GetMapping
+    public ResponseEntity<String> sampleFunction(){
+        return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Integer> makeRecord(@RequestBody RecVO rec){
+        if(service.insertRec(rec))
+            return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
+        else
+            return new ResponseEntity<Integer>(FAIL, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @PostMapping("/start")
+    public ResponseEntity<Integer> startRecord(@RequestBody DayVO day){
+        if(service.insertDayRec(day))
+            return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
+        else
+            return new ResponseEntity<Integer>(FAIL, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @PostMapping("/route")
+    public ResponseEntity<Integer> saveRoute(@RequestBody RouteVO pos){
+        if(service.insertRoute(pos))
+            return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
+        else
+            return new ResponseEntity<Integer>(FAIL, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @PostMapping("/receipt")
+    public ResponseEntity<Integer> saveConsume(@RequestBody ConsumeVO consume){
+        if(service.insertConsumption(consume))
+            return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
+        else
+            return new ResponseEntity<Integer>(FAIL, HttpStatus.NOT_ACCEPTABLE);
+    }
+
+    @GetMapping("/time")
+    public ResponseEntity<Integer> getRecordTime(){
+        return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
+    }
+
+    @GetMapping("/distance")
+    public ResponseEntity<Integer> getTotalDistance(){
+        return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
+    }
+
+    @GetMapping("/receipt")
+    public ResponseEntity<Integer> getConsume(){
+        return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
+    }
+
+    @GetMapping("/pictures")
+    public ResponseEntity<Integer> getPictures(){
+        return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
     }
 }
--- backend/src/main/java/ssafy/backend/afterglow/service/RecordService.java
+++ backend/src/main/java/ssafy/backend/afterglow/service/RecordService.java
@@ -2,12 +2,89 @@ package ssafy.backend.afterglow.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ssafy.backend.afterglow.repository.RecordRepository;
+import ssafy.backend.afterglow.VO.ConsumeVO;
+import ssafy.backend.afterglow.VO.DayVO;
+import ssafy.backend.afterglow.VO.RecVO;
+import ssafy.backend.afterglow.VO.RouteVO;
+import ssafy.backend.afterglow.domain.ConsumptionRecord;
+import ssafy.backend.afterglow.domain.DailyRecord;
+import ssafy.backend.afterglow.domain.Record;
+import ssafy.backend.afterglow.domain.RouteRecord;
+import ssafy.backend.afterglow.repository.*;
+
+import java.util.List;
 
 @Service
 public class RecordService {
     @Autowired
     RecordRepository repo;
 
+    @Autowired
+    DailyRepository dayRepo;
+
+    @Autowired
+    RouteRepository rouRepo;
+
+    @Autowired
+    ConsumptionRepository conRepo;
+
+    @Autowired
+    PinRepository pinRepo;
+
+    @Autowired
+    ImageRepository imgRepo;
+
+    public boolean insertRec(RecVO vo){
+        try{
+            Record rec = Record.builder().user(vo.getUser()).recName(vo.getRecName()).build();
+            repo.save(rec);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean insertDayRec(DayVO vo){
+        try{
+            DailyRecord rec = DailyRecord.builder().rec(vo.getRec()).drStartTime(vo.getStartTime()).build();
+            dayRepo.save(rec);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean insertRoute(RouteVO vo){
+        try{
+            RouteRecord rec = RouteRecord.builder().dr(vo.getDr()).rrLatitude(vo.getLatitude()).rrLongitude(vo.getLongitude()).build();
+            rouRepo.save(rec);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean insertConsumption(ConsumeVO vo){
+        try{
+            ConsumptionRecord rec = ConsumptionRecord.builder().crName(vo.getName()).crMoney(vo.getMoney()).crDatetime(vo.getDateTime()).build();
+            conRepo.save(rec);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public String getRecTotalTime(Integer recId){
+        // dr.drStartTime
+        // dr.drEndTime
+        // Period - 날짜 차이
+        // Duration - 시간 차이
+
+        return null;
+    }
 
 }
