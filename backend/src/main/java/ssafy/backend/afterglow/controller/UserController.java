d0f89961022af7fa478459c598daf4a3a2c43c5a
YeonggilGo <dudrlf1859@naver.com>
2021-04-30 17:24:32 +0900

    Update UserController.java

--- backend/src/main/java/ssafy/backend/afterglow/controller/UserController.java
+++ backend/src/main/java/ssafy/backend/afterglow/controller/UserController.java
@@ -1,10 +1,13 @@
 package ssafy.backend.afterglow.controller;
 
+import io.swagger.models.Model;
+import org.springframework.core.ResolvableType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2RefreshToken;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -64,5 +67,28 @@ public class UserController {
         }
 
         return ResponseEntity.ok(principal);
+    }
+
+    private static final String authorizationRequestBaseUri = "oauth2/authorization";
+    Map<String, String> oauth2AuthenticationUrls = new HashMap<>();
+    // Lombok 아닌 경우 (@RequiredArgsConstructor 없는 경우)
+    // @Autowired private ClientRegistrationRepository clientRegistrationRepository;
+    @SuppressWarnings("unchecked")
+    @GetMapping("/login")
+    public String getLoginPage(Model model) throws Exception {
+        Iterable<ClientRegistration> clientRegistrations = null;
+        ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository)
+                .as(Iterable.class);
+        if (type != ResolvableType.NONE &&
+                ClientRegistration.class.isAssignableFrom(type.resolveGenerics()[0])) {
+            clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;
+        }
+        assert clientRegistrations != null;
+        clientRegistrations.forEach(registration ->
+                oauth2AuthenticationUrls.put(registration.getClientName(),
+                        authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+
+        return "auth/oauth-login";
     }
 }
