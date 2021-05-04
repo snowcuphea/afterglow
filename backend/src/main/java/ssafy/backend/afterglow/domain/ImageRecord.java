44a4f32434ad45dc34f5c2d2a21503247fe39a44
halo2281@naver.com <halo2281@naver.com>
2021-05-04 18:34:52 +0900

    [EDIT] Change Variable Type
    Blob 대신 byte 배열을 이용

--- backend/src/main/java/ssafy/backend/afterglow/domain/ImageRecord.java
+++ backend/src/main/java/ssafy/backend/afterglow/domain/ImageRecord.java
@@ -14,17 +14,16 @@ public class ImageRecord {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty("pr_id")
     private Long imgId;
-    @Lob @JsonProperty("ir_image")
-    private Blob irImage;
+    @JsonProperty("ir_image")
+    @Lob
+    private byte[] irImage;
 
     @ManyToOne
     @JoinColumn(name = "rrId")
     @JsonProperty("rr")
     private RouteRecord rr;
 
-    @JsonProperty("ir_image")
-    @Lob
-    private byte[] irImage;
+
 
     @Builder
     public ImageRecord(RouteRecord rr, byte[] irImage){
