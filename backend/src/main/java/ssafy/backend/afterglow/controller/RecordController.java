111d61cf167a03d2544102c5bf0fb52473a1d152
YeonggilGo <dudrlf1859@naver.com>
2021-05-04 17:09:26 +0900

    [ADD] get all record

--- backend/src/main/java/ssafy/backend/afterglow/controller/RecordController.java
+++ backend/src/main/java/ssafy/backend/afterglow/controller/RecordController.java
@@ -1,12 +1,41 @@
 package ssafy.backend.afterglow.controller;
 
+<<<<<<< Updated upstream
+=======
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+>>>>>>> Stashed changes
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.*;
+<<<<<<< Updated upstream
 import ssafy.backend.afterglow.VO.*;
 import ssafy.backend.afterglow.service.RecordService;
 
+=======
+import org.springframework.web.multipart.MultipartFile;
+import ssafy.backend.afterglow.domain.ImageRecord;
+import ssafy.backend.afterglow.repository.ImageRepository;
+import ssafy.backend.afterglow.repository.RecordRepository;
+import ssafy.backend.afterglow.repository.UserRepository;
+import ssafy.backend.afterglow.service.RecordService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.List;
+
+import ssafy.backend.afterglow.domain.Record;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+>>>>>>> Stashed changes
 @RestController
 @RequestMapping("records")
 public class RecordController {
@@ -16,6 +45,13 @@ public class RecordController {
     @Autowired
     RecordService service;
 
+<<<<<<< Updated upstream
+=======
+    private final ImageRepository imagerepository;
+    private final UserRepository userRepository;
+    private final RecordRepository recordRepository;
+
+>>>>>>> Stashed changes
     @GetMapping
     public ResponseEntity<String> sampleFunction(){
         return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
@@ -72,4 +108,21 @@ public class RecordController {
     public ResponseEntity<Integer> getPictures(){
         return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
     }
+
+    // 유저 전체 여행
+    @GetMapping("/")
+    public ResponseEntity<Object> currentUserToken(@AuthenticationPrincipal Principal principal) {
+        AtomicReference<List<Record>> result = null;
+        if (principal instanceof OAuth2AuthenticationToken) {
+            Map<String, Object> attributes = new HashMap<>();
+            OAuth2AuthenticationToken oAuth2AuthenticationToken = (OAuth2AuthenticationToken) principal;
+            userRepository.findByUsername(principal.getName())
+                    .ifPresent(user -> {
+                        result.set(recordRepository.findByUser(user));
+                    });
+        }
+        return ResponseEntity.ok(result);
+    }
+
+
 }
