9072df41ad43f6c3d1ad869aefc91b422ba91cb3
snowcuphea <snowcuphea@naver.com>
2021-05-08 20:14:16 +0900

    [ADD] 가계부 아이템 추가 정규식/예외처리 적용

--- frontend/src/components/book/AddMoneyItem.js
+++ frontend/src/components/book/AddMoneyItem.js
@@ -17,6 +17,9 @@ class AddMoneyItem extends React.Component {
       nowMin:'',
       nowWhat:'',
       nowMuch:'',
+      timeValid: true,
+      whatValid: false,
+      muchValid: false,
     }
   }
 
@@ -32,15 +35,42 @@ class AddMoneyItem extends React.Component {
 
   setHour = (t) => {
     this.setState({ nowHour: t });
+    let pattern = /^([1-9]|[01][0-9]|2[0-3])$/;
+    if (!pattern.test(t)){
+      console.log("잘못된 시간(시).")
+      this.setState({ timeValid: false });
+    }else {
+      this.setState({ timeValid: true });
+    }
   }
   setMin = (t) => {
     this.setState({ nowMin: t });
+    let pattern = /^([0-5][0-9])$/;
+    if (!pattern.test(t)){
+      console.log("잘못된 시간(분).")
+      this.setState({ timeValid: false });
+    }else {
+      this.setState({ timeValid: true });
+    }
   }
   setWhat = (t) => {
     this.setState({ nowWhat: t });
+    if (t.trim().length === 0) {
+      console.log("잘못된 사용처")
+      this.setState({ whatValid: false });
+    } else {
+      this.setState({ whatValid: true });
+    }
   }
   setMuch = (t) => {
     this.setState({ nowMuch: t });
+    let pattern = /[0-9]/;
+    if (!pattern.test(t)) {
+      console.log("잘못된금액")
+      this.setState({ muchValid: false });
+    } else {
+      this.setState({ muchValid: true });
+    }
   }
 
   addItem = () => {
@@ -87,6 +117,9 @@ class AddMoneyItem extends React.Component {
             placeholder='사용처'
             value={this.state.nowWhat}
             onChangeText={(t) => this.setWhat(t)}
+            onEndEditting={() => console.log("onEndEditting")}
+            onSubmitEditing={() => console.log("onSubmitEditing")}
+            returnKeyType="next"
             style={{fontSize:15}} />
         </View>
         <View>
@@ -97,9 +130,16 @@ class AddMoneyItem extends React.Component {
             onChangeText={(t) => this.setMuch(t)}
             keyboardType = 'numeric' />
         </View>
+        { this.state.timeValid
+        ? ( this.state.whatValid ? (this.state.muchValid ? <Text>OK</Text> : <Text>가격을 적어주세요.</Text> ) : <Text>사용처를 적어주세요.</Text> ) 
+        : <Text>올바른 시간을 적어주세요. </Text>
+        }
         <TouchableOpacity
-          style={{backgroundColor:'orange', alignItems: 'center'}}
-          onPress={this.addItem} >
+          style={{backgroundColor:'beige', alignItems: 'center'}}
+          onPress={ (this.state.timeValid && this.state.whatValid && this.state.muchValid)
+          ? this.addItem
+          : ()=> {console.log("가계부추가실패")}}
+          >
           <Text>추가</Text>
         </TouchableOpacity>
       </View>
