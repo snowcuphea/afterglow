bfacd1240f619546a2d9955850ca45fed2684f25
halo2281@naver.com <halo2281@naver.com>
2021-05-10 14:41:07 +0900

    [EDIT] Fix Controller

--- backend/src/main/java/ssafy/backend/afterglow/controller/RecordController.java
+++ backend/src/main/java/ssafy/backend/afterglow/controller/RecordController.java
@@ -9,7 +9,6 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import ssafy.backend.afterglow.domain.*;
 import ssafy.backend.afterglow.dto.ImageInputDto;
-import ssafy.backend.afterglow.dto.RecordDTO;
 import ssafy.backend.afterglow.repository.*;
 import ssafy.backend.afterglow.service.RecordService;
 import ssafy.backend.afterglow.service.UserService;
@@ -22,7 +21,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
 
 @RestController
 @RequestMapping("records")
@@ -34,6 +32,7 @@ public class RecordController {
     private final RecordService recordService;
     private final UserService userService;
 
+    private final UserRepository userRepository;
     private final ImageRepository imageRepository;
     private final RecordRepository recordRepository;
     private final DailyRepository dailyRepository;
@@ -66,35 +65,26 @@ public class RecordController {
         return new ResponseEntity<Integer>(SUCCESS, HttpStatus.OK);
     }
 
-    // 여행 선택 시 여행 정보 받아오기
-    @GetMapping
-    public ResponseEntity<Object> getRecord(@RequestParam("record_id") Long recId) {
-        RecordDTO test = recordService.selectRecordInfo(recId);
-        if (test != null)
-            return new ResponseEntity<Object>(test, HttpStatus.OK);
-        else
-            return new ResponseEntity<Object>(FAIL, HttpStatus.NOT_ACCEPTABLE);
-    }
-
-
     // 여행 시작
     @PostMapping("/startTrip")
     public ResponseEntity<String> startTrip(@AuthenticationPrincipal Principal principal,
                                             @RequestParam("title") String recTitle) {
-        AtomicReference<Record> record = null;
-        AtomicReference<DailyRecord> dr = null;
+        var ref = new Object() {
+            Record record = null;
+            DailyRecord dr = null;
+        };
 
         userService
                 .findUserByPrincipal(principal)
                 .ifPresent(user -> {
-                    record.set(recordRepository.save(Record.builder()
+                    ref.record = recordRepository.save(Record.builder()
                             .user(user)
                             .recName(recTitle)
-                            .build()));
-                    dr.set(dailyRepository.save(DailyRecord.builder()
-                            .rec(record.get())
+                            .build());
+                    ref.dr = dailyRepository.save(DailyRecord.builder()
+                            .rec(ref.record)
                             .drStartTime(LocalDateTime.now())
-                            .build()));
+                            .build());
                 });
         return ResponseEntity.ok(principal.getName());
     }
@@ -106,18 +96,20 @@ public class RecordController {
                                                  @RequestParam("consumption_name") String conName,
                                                  @RequestParam("consumption_money") Integer conMoney,
                                                  @RequestParam("consumption_time") LocalDateTime conTime) {
-        AtomicReference<ConsumptionRecord> result = null;
+        var ref = new Object() {
+            ConsumptionRecord result = null;
+        };
         dailyRepository
                 .findById(dayId)
                 .ifPresent(dr -> {
-                    result.set(conRepository.save(ConsumptionRecord.builder()
+                    ref.result = conRepository.save(ConsumptionRecord.builder()
                             .dr(dr)
                             .crName(conName)
                             .crMoney(conMoney)
                             .crDatetime(conTime)
-                            .build()));
+                            .build());
                 });
-        return ResponseEntity.ok(result.get());
+        return ResponseEntity.ok(ref.result);
     }
 
 
@@ -127,23 +119,24 @@ public class RecordController {
                                                     @RequestParam("consumption_name") String conName,
                                                     @RequestParam("consumption_money") Integer conMoney,
                                                     @RequestParam("consumption_time") @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime conTime) {
-        AtomicReference<ConsumptionRecord> result = null;
+        var ref = new Object() {
+            ConsumptionRecord result = null;
+        };
         conRepository
                 .findById(conId)
                 .ifPresent(cr -> {
-                    result.set(conRepository.save(ConsumptionRecord.builder()
+                    ref.result = conRepository.save(ConsumptionRecord.builder()
                             .crName(conName)
                             .crMoney(conMoney)
                             .crDatetime(conTime)
-                            .build()));
+                            .build());
                 });
-        return ResponseEntity.ok(result.get());
+        return ResponseEntity.ok(ref.result);
     }
 
     // 가계부 삭제
     @DeleteMapping("/consumption")
     public ResponseEntity<Object> deleteConsumption(@RequestParam("consumption_id") Long conId){
-        AtomicReference<ConsumptionRecord> result = null;
         conRepository
                 .findById(conId)
                 .ifPresent(cr -> {
@@ -155,30 +148,34 @@ public class RecordController {
 
     // 유저 전체 여행
     @GetMapping("/total")
-    public ResponseEntity<Object> currentUserToken(@AuthenticationPrincipal Principal principal) {
-        AtomicReference<List<Record>> result = null;
-        userService
-                .findUserByPrincipal(principal)
+    public ResponseEntity<Object> currentUserToken(@RequestParam("user_id") Long userId) {
+        var ref = new Object() {
+            List<Record> result = null;
+        };
+        userRepository
+                .findById(userId)
                 .ifPresent(user -> {
-                    result.set(recordRepository.findByUser(user));
+                    ref.result = recordRepository.findByUser(user);
                 });
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ref.result);
     }
 
     // 하루 기준 현 시간까지의 실시간 정보 받아오기
     @GetMapping("/current")
     public ResponseEntity<Object> currentInfo(@AuthenticationPrincipal Principal principal,
                                               @RequestParam("date") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate today) {
-        AtomicReference<DailyRecord> result = null;
+        var ref = new Object() {
+            DailyRecord result = null;
+        };
         userService
                 .findUserByPrincipal(principal)
                 .ifPresent(user -> {
                     dailyRepository.findByDrDateAndRec_User(today, user)
                             .ifPresent(dr -> {
-                                result.set(dr);
+                                ref.result = dr;
                             });
                 });
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ref.result);
     }
 
 
@@ -201,29 +198,33 @@ public class RecordController {
     public ResponseEntity<Object> saveRoute(@RequestParam("dr_id") Long drId,
                                              @RequestParam("rr_latitude") Double rrLat,
                                              @RequestParam("rr_longitude") Double rrLong){
-        AtomicReference<RouteRecord> result = null;
+        var ref = new Object() {
+            RouteRecord result = null;
+        };
         dailyRepository.findById(drId)
                 .ifPresent(dr -> {
-                    result.set(routeRepository.save(RouteRecord.builder()
+                    ref.result =routeRepository.save(RouteRecord.builder()
                             .dr(dr)
                             .rrLatitude(rrLat)
                             .rrLongitude(rrLong)
-                            .build()));
+                            .build());
                 });
-        return ResponseEntity.ok(result.get());
+        return ResponseEntity.ok(ref.result);
     }
 
     // 경로 이름 작성
     @PostMapping("/route/name")
     public ResponseEntity<RouteRecord> routeNaming(@RequestParam("route_name") String routeName,
                                                    @RequestParam("Rr_id") Long RrId) {
-        AtomicReference<RouteRecord> result = null;
+        var ref = new Object() {
+            RouteRecord result = null;
+        };
         routeRepository.findById(RrId)
                 .ifPresent(rr -> {
                     rr.setRrName(routeName);
-                    result.set(rr);
+                    ref.result = rr;
                 });
-        return ResponseEntity.ok(result.get());
+        return ResponseEntity.ok(ref.result);
     }
 
 
@@ -231,20 +232,24 @@ public class RecordController {
     @PostMapping("/memo/create")
     public ResponseEntity<RouteRecord> addAndUpdateMemo(@RequestParam("Rr_id") Long RrId,
                                                         @RequestParam("memo_content") String memoContent) {
-        AtomicReference<RouteRecord> result = null;
+        var ref = new Object() {
+            RouteRecord result = null;
+        };
         routeRepository
                 .findById(RrId)
                 .ifPresent(rr -> {
                     rr.setRrMemo(memoContent);
-                    result.set(rr);
+                    ref.result = rr;
                 });
-        return ResponseEntity.ok(result.get());
+        return ResponseEntity.ok(ref.result);
     }
 
     // 하루 사진
     @GetMapping("/daily/picture")
     public ResponseEntity<List<ImageRecord>> dailyPicture(@AuthenticationPrincipal Principal principal) {
-        List<ImageRecord> result = null;
+        var ref = new Object() {
+            List<ImageRecord> result = null;
+        };
         userService
                 .findUserByPrincipal(principal)
                 .ifPresent(user -> {
@@ -252,12 +257,12 @@ public class RecordController {
                             .ifPresent(dr -> {
                                 routeRepository.findByDr(dr)
                                         .forEach(rr -> {
-                                            result.addAll(imageRepository.findAllByRr(rr)
+                                            ref.result.addAll(imageRepository.findAllByRr(rr)
                                                     .orElse(new ArrayList<>()));
                                         });
                             });
                 });
-        return ResponseEntity.ok(result);
+        return ResponseEntity.ok(ref.result);
     }
 
 
