e5eaa86701ff268ad8e9af17fa96e30d85bf7447
snowcuphea <snowcuphea@naver.com>
2021-05-10 21:04:17 +0900

    [EDIT] getRecordList api 추가

--- frontend/src/api/account.js
+++ frontend/src/api/account.js
@@ -1,3 +1,17 @@
 import { createInstance } from "./index.js"
 
 const instance = createInstance()
+
+
+function getRecordList( success, fail ) {
+
+    // instance.defaults.headers['Authorization'] = "jwt " + window.localStorage.getItem('jwt')
+
+    instance
+      .get("records/total")
+      .then(success)
+      .catch(fail)
+  }
+  
+  export { getRecordList }
+  
