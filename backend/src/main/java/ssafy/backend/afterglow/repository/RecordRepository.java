1cb75cd1d8a44867eeee862fe4866276a28e6155
halo2281@naver.com <halo2281@naver.com>
2021-05-10 14:40:56 +0900

    [EDIT] Delete Unused Functions

--- backend/src/main/java/ssafy/backend/afterglow/repository/RecordRepository.java
+++ backend/src/main/java/ssafy/backend/afterglow/repository/RecordRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ssafy.backend.afterglow.domain.Record;
 import ssafy.backend.afterglow.domain.User;
+
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long> {
