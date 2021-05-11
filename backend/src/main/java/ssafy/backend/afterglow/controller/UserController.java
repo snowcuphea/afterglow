b7a86ed64a91129ad2a820dfba3a7548e37e71d4
YeonggilGo <dudrlf1859@naver.com>
2021-05-11 16:35:30 +0900

    Update UserController.java

--- backend/src/main/java/ssafy/backend/afterglow/controller/UserController.java
+++ backend/src/main/java/ssafy/backend/afterglow/controller/UserController.java
@@ -11,6 +11,7 @@ import ssafy.backend.afterglow.service.UserService;
 import javax.servlet.http.HttpServletRequest;
 import javax.transaction.Transactional;
 import java.io.IOException;
+import java.util.Arrays;
 
 @RestController
 public class UserController {
@@ -21,8 +22,10 @@ public class UserController {
     @GetMapping("/customLogin")
     @Transactional
     public ResponseEntity<Object> login(HttpServletRequest request) throws IOException {
-        System.out.println(request.getCookies());
-        System.out.println(request.getHeader("Cookies"));
+        Arrays.stream(request.getCookies())
+                .forEach(cookie -> System.out.println(cookie));
+
+        System.out.println(request.getHeaders("Cookies").toString());
         User user = userService.login(request);
         return ResponseEntity.ok(user.getUsername());
     }
