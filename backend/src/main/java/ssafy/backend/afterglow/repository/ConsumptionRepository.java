1b4946882ded7353140c608267fdd050f03694e4
YeonggilGo <dudrlf1859@naver.com>
2021-05-17 16:29:28 +0900

    [EDIT] create bean repo

--- backend/src/main/java/ssafy/backend/afterglow/repository/ConsumptionRepository.java
+++ backend/src/main/java/ssafy/backend/afterglow/repository/ConsumptionRepository.java
@@ -1,6 +1,7 @@
 package ssafy.backend.afterglow.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ssafy.backend.afterglow.domain.ConsumptionRecord;
 import ssafy.backend.afterglow.domain.DailyRecord;
 import ssafy.backend.afterglow.domain.Record;
@@ -8,6 +9,7 @@ import ssafy.backend.afterglow.domain.Record;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ConsumptionRepository extends JpaRepository<ConsumptionRecord, Long> {
     Optional<List<ConsumptionRecord>> findAllByDr(DailyRecord dr);
 }
