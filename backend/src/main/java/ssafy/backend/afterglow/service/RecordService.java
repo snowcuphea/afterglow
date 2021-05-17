434ff6bae3a6d0a1a9e321f6040f76806e22377a
YeonggilGo <dudrlf1859@naver.com>
2021-05-17 14:22:01 +0900

    Update RecordService.java

--- backend/src/main/java/ssafy/backend/afterglow/service/RecordService.java
+++ backend/src/main/java/ssafy/backend/afterglow/service/RecordService.java
@@ -54,7 +54,7 @@ public class RecordService {
 
     public Optional<RouteRecord> getLatestRr(DailyRecord dr) {
         Optional<List<RouteRecord>> rrList = rouRepo.findByDr(dr);
-        if (rrList == null) {
+        if (rrList == null || rrList.get().size() == 0) {
             return null;
         } else {
             return Optional.ofNullable(rrList.get().get(rrList.get().size() - 1));
