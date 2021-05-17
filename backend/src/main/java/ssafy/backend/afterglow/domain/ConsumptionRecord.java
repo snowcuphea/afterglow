e10dfa02235259fbe0ae038e760d274117dbff6d
YeonggilGo <dudrlf1859@naver.com>
2021-05-17 16:00:01 +0900

    [EDIT] delete rememberme

--- backend/src/main/java/ssafy/backend/afterglow/domain/ConsumptionRecord.java
+++ backend/src/main/java/ssafy/backend/afterglow/domain/ConsumptionRecord.java
@@ -15,17 +15,19 @@ public class ConsumptionRecord {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty("cr_id")
     private Long crId;
+
     @JsonProperty("cr_name")
     private String crName;
+
     @JsonProperty("cr_money")
     private Integer crMoney;
+
     @JsonProperty("cr_datetime")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd 'T'HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime crDatetime;
 
     @ManyToOne
     @JoinColumn(name = "drId")
-    @JsonProperty("dr")
     @JsonIgnore
     private DailyRecord dr;
 
